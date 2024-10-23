ee5db182cbc2212f3531357c7e5c14a053f7f412
Vali Repciuc <valirepciuc@Valis-MacBook-Pro.local>
2024-10-23 17:17:41 +0300

    SIMPLE_JWT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    axios.get('/api/')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/users/2/`);
+        setUser(response.data); // Axios automatically parses JSON
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchUser();
   }, []);
 
   return (
     <div className="App">
-      <h1>{message || "Loading..."}</h1>
+      <h1>{user ? user.username : "Loading..."}</h1>
     </div>
   );
 }
